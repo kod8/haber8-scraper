1d6bfd0d10a1a519c403971c7fadfc4332fcafeb
Abdullah <abdullahoguk@gmail.com>
2020-12-08 17:06:34 +0300

    initial structure

---
+++ urls.js
@@ -0,0 +1,17 @@
+const PUAN_URLS = {
+    "superLig": "https://www.haberturk.com/spor/iddaa/puandurumu/1/turkiye-super-lig",
+    "l3g4": "https://www.haberturk.com/spor/iddaa/puandurumu/63"
+}
+
+const const FIKSTUR_URLS = {
+    "superLig": "https://www.haberturk.com/spor/fikstur/1-turkiye-super-lig",
+    "l3g4": "https://www.haberturk.com/spor/iddaa/puandurumu/63"
+}
+
+const const PUAN_FIKSTUR_SONUC_URLS = {
+    "superLig": "https://sporzip.com/super-lig",
+    "l3g4": "https://sporzip.com/3-lig-4"
+}
+
+
+export {PUAN_URLS, FIKSTUR_URLS, PUAN_FIKSTUR_URLS}
