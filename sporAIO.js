08f6b97dd148c7bb9105fdf06e39d84940452cb2
Abdullah <abdullahoguk@gmail.com>
2020-12-13 14:41:52 +0300

    convert scoreboard to json

--- sporAIO.js
+++ sporAIO.js
@@ -21,32 +21,32 @@ const {fetchAsync} = require('./utils.js');
         
         //PUAN
         var puanHTML = removeLinksInHTML(fullDOM.querySelector("#puan-durumu table"))
+        
+
         writeFile(puanHTML.toString(),ligID,"./data/spor/html/puan/","html");
         sporData[ligID].puanHTML = puanHTML.toString();
-        //puanJSON = puanObj(puanHTML);
-        //sporData[ligID].puanJSON = puanJSON;
+        puanJSON = puanObj(puanHTML);
+        writeFile(puanJSON ,ligID,"./data/spor/json/puan/","json");
+        sporData[ligID].puanJSON = puanJSON;
 
         //FIKSTUR
         var fiksturHTML = removeLinksInHTML(fullDOM.querySelectorAll("table.liste.cs_LF")[0])
         writeFile(fiksturHTML.toString(),ligID,"./data/spor/html/fikstur/","html");
         sporData[ligID].fiksturHTML = fiksturHTML.toString();
         //fiksturJSON = fiksturObj(fiksturHTML);
+        //writeFile(fiksturJSON ,ligID,"./data/spor/json/fikstur/","json");
         //sporData[ligID].fiksturJSON = fiksturJSON;
-
 
         //SONUC        
         var sonucHTML = removeLinksInHTML(fullDOM.querySelectorAll("table.liste.cs_LF")[1]);
         writeFile(sonucHTML.toString(),ligID,"./data/spor/html/sonuc/","html");
         sporData[ligID].sonucHTML = sonucHTML.toString();
         //sonucJSON = puanObj(sonucHTML);
+        //writeFile(sonucJSON ,ligID,"./data/spor/json/sonuc/","json");
         //sporData[ligID].sonucJSON = sonucJSON;
     }
-
+    //TODO : Send POST KOD8 API 
     writeFile(sporData,"spor","./data/spor/","json");
-   /*
-    var logText = `${new Date().toGMTString()}: Spor dosyaları güncellendi`
-    writeFile(logText,"logs","./","log");
-    */
 }
 
 
@@ -64,24 +64,49 @@ function removeLinksInHTML(element){
             td.set_content(td.querySelector("a").innerText);
         }
     })
-
     return element;
 }
 
 
-// TODO: PUANOBJ func
-function puanObj(){
+//  Convert puan table HTML to JSON
+function puanObj(puanHTML){
+    var [thead,...tbody] = puanHTML.querySelectorAll("tr")
+    var obj = {}
+    obj.tableTitle = puanHTML.querySelector("caption h2").innerText;
+    obj.columns = [...thead.querySelectorAll("th")].map(col=>col.innerText);
+    obj.list = [];
 
+    tbody.forEach(function(team){
+        var listItemObj = {};
+        var keys = obj.columns;
+        team.querySelectorAll("td").forEach((cell,index)=>{
+            listItemObj[keys[index]] = cell.innerText
+        })
+        obj.list.push(listItemObj);
+    })
+
+    puanHTML.querySelectorAll("tr:not(thead tr)").forEach(function(tr){
+        console.log(tr.toString())
+    })
+
+    return obj;
 }
 
 // TODO: FiksturOBJ func
 function fiksturObj(){
+    var obj = {}
 
+
+    return obj;
 }
 
 // TODO: PUANOBJ func
 function sonucObj(){
+    var obj = {}
 
+
+    
+    return obj;
 }
 
 
