35b674e2b3fb617cb363757c9a79ea2123bf025e
Abdullah <abdullahoguk@gmail.com>
2020-12-15 17:22:23 +0300

    Update urls.js

--- urls.js
+++ urls.js
@@ -11,6 +11,9 @@ const FIKSTUR_URLS = {
 const PUAN_FIKSTUR_SONUC_URLS = {
     "superLig": {"name":"Süper Lig", "url":"https://sporzip.com/super-lig"},
     "l3g4": {"name":"3. Lig 4. Grup", "url":"https://sporzip.com/3-lig-4"},
+    "l3g1": {"name":"3. Lig 1. Grup", "url":"https://sporzip.com/3-lig-1"},
+    "l3g2": {"name":"3. Lig 2. Grup", "url":"https://sporzip.com/3-lig-2"},
+    "l3g3": {"name":"3. Lig 3. Grup", "url":"https://sporzip.com/3-lig-3"},
     "l1": {"name":"Birinci Lig", "url":"https://sporzip.com/1-lig"},
     "l2k": {"name":"2. Lig Kırmızı Grup", "url":"https://sporzip.com/2-lig-kirmizi"},
     "l2b": {"name":"2. Lig Beyaz Grup", "url":"https://sporzip.com/2-lig-beyaz"},
